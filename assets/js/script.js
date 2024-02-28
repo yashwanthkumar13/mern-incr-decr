01485025ec19b374853fc68a97f296d46efe6237
yashwanthkumar13 <yashwanthyhr@gmail.com>
2024-02-28 21:04:05 +0530

    Initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
+const counterElement = document.getElementById('counter-number');
+
 const incrementFunction = () => {
-    console.log(`increment logged!`)
+  console.log('increment logged!');
+  const currentValue = parseInt(counterElement.textContent);
+  counterElement.textContent = currentValue + 1;
 }
 
 const decrementFunction = () => {
-    console.log(`decrement logged!`)
+  console.log('decrement logged!');
+  const currentValue = parseInt(counterElement.textContent, 10);
+  counterElement.textContent = currentValue - 1;
 }
